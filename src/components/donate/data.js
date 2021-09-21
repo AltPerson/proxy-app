fc01ee51ecf300ab3140499e81622e090980ad51
Igor <matviychukigor@gmail.com>
2021-09-21 23:21:07 +0300

    donat-start_functional(work-with_btn)

---
+++ src/components/donate/data.js
@@ -0,0 +1,32 @@
+export const buttonsQIWI = [
+    {
+        id: "1",
+        name: "Russian",
+        emoji: "🇷🇺", 
+        chek: false
+    },
+    {
+        id: "2",
+        name: "Ukraine",
+        emoji: "🇺🇦",
+        chek: false
+    },
+    {
+        id: "3",
+        name: "Dollar",
+        emoji: null,
+        chek: false
+    },
+    {
+        id: "4",
+        name: "Euro",
+        emoji: null,
+        chek: false
+    },
+    {
+        id: "5",
+        name: "Kazahstan",
+        emoji: "🇰🇿", 
+        chek: false
+    }
+];
