8635d2590d327653646ab4aab2218dc646841337
Igor <matviychukigor@gmail.com>
2021-09-30 17:00:26 +0300

    proxyc

--- src/components/land/agree.component.js
+++ src/components/land/agree.component.js
@@ -1,8 +1,8 @@
 import React from "react";
-import tickSvg from "./agreetick.svg";
+import tickSvg from "./ghost.ico";
 
 function Agree() {
-  return <img className="tick-image" src={tickSvg} alt="tick-img" />;
+  return <img style={{marginTop: "3px"}} className="tick-image" src={tickSvg} alt="tick-img" />;
 }
 
 export default Agree;
