407a3b3b559c683089a26936e8ed359eacdee2ee
Igor <matviychukigor@gmail.com>
2021-10-04 12:32:24 +0300

    update search2

--- src/components/home/data.js
+++ src/components/home/data.js
@@ -45,7 +45,7 @@ export const dropDownValueLeft = [
   {
     id: "3",
     name: "Datacenter/hosting",
-    value: "Datacenter/hosting",
+    value: "Datacenter%2Fhosting",
   },
   {
     id: "4",
